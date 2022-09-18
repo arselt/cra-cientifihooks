9d43b4ff36c293a104c3b82fbe11c799f752498b
Arath Astorga <xarselt@gmail.com>
2022-09-17 23:52:00 -0500

    Add: feature, favorite button and list

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,24 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useReducer } from "react"
 import "./Characters.css";
+
+const initialState = {
+    favorites: []
+}
+
+const favoriteReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TO_FAVORITE':
+            return {
+                ...state,
+                favorites: [...state.favorites, action.payload]
+            };
+        default: return state;
+    }
+}
 
 const Characters = () => {
     const [characters, setCharacters] = useState([]);
+    const [favorite, dispatch] = useReducer(favoriteReducer, initialState);
 
     useEffect(() => {
         fetch("https://rickandmortyapi.com/api/character/")
@@ -10,10 +26,14 @@ const Characters = () => {
             .then(data => setCharacters(data.results))
     }, []);
 
+    const handleClick = favorite => {
+        dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
+    }
+
     return (
         <div className="characters">
             {characters.map(character => (
-                <div className="character-card">
+                <div className="character-card" key={character.id}>
                     <img
                         className="card-img"
                         src={character.image}
@@ -24,9 +44,26 @@ const Characters = () => {
                         <li><h3>{character.species}</h3></li>
                         <li><p>{character.origin.name}</p></li>
                         <li><p>{character.episode.length} episodes</p></li>
+                        <li>
+                            <button
+                                type="button"
+                                onClick={() => handleClick(character)}
+                            >
+                                🤍
+                            </button>
+                        </li>
                     </ul>
                 </div>
             ))}
+            <div>
+                <ul className="favorites-card">
+                    {favorite.favorites.map(favorite => (
+                        <li key={favorite.id}>
+                            {favorite.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
