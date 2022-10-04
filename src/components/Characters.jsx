de0580cf38493880893de455d49b782f1c1ebce8
Arath Astorga <xarselt@gmail.com>
2022-10-03 20:20:48 -0500

    Add: feature, searchbar

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useReducer } from "react"
+import React, { useState, useEffect, useReducer, useMemo, useRef, useCallback } from "react"
 import "./Characters.css";
+import Search from "./Search";
 
 const initialState = {
     favorites: []
@@ -19,6 +20,8 @@ const favoriteReducer = (state, action) => {
 const Characters = () => {
     const [characters, setCharacters] = useState([]);
     const [favorite, dispatch] = useReducer(favoriteReducer, initialState);
+    const [search, setSearch] = useState('');
+    const searchInput = useRef(null);
 
     useEffect(() => {
         fetch("https://rickandmortyapi.com/api/character/")
@@ -30,41 +33,59 @@ const Characters = () => {
         dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
     }
 
+    // const handleSearch = () => {
+    //     setSearch(searchInput.current.value);
+    // }
+
+    const handleSearch = useCallback(() => {
+        setSearch(searchInput.current.value);
+    }, [])
+
+    const filteredUsers = useMemo(() =>
+        characters.filter((user) => {
+            return user.name.toLowerCase().includes(search.toLowerCase());
+        }),
+        [characters, search]
+    )
+
     return (
-        <div className="characters">
-            {characters.map(character => (
-                <div className="character-card" key={character.id}>
-                    <img
-                        className="card-img"
-                        src={character.image}
-                        alt="{character.name}"
-                    />
-                    <ul className="card-info">
-                        <li><h2>{character.name}</h2></li>
-                        <li><h3>{character.species}</h3></li>
-                        <li><p>{character.origin.name}</p></li>
-                        <li><p>{character.episode.length} episodes</p></li>
-                        <li>
-                            <button
-                                type="button"
-                                onClick={() => handleClick(character)}
-                            >
-                                🤍
-                            </button>
-                        </li>
+        <>
+            <Search search={search} searchInput={searchInput} handleSearch={handleSearch} />
+            <div className="characters">
+                {filteredUsers.map(character => (
+                    <div className="character-card" key={character.id}>
+                        <img
+                            className="card-img"
+                            src={character.image}
+                            alt="{character.name}"
+                        />
+                        <ul className="card-info">
+                            <li><h2>{character.name}</h2></li>
+                            <li><h3>{character.species}</h3></li>
+                            <li><p>{character.origin.name}</p></li>
+                            <li><p>{character.episode.length} episodes</p></li>
+                            <li>
+                                <button
+                                    type="button"
+                                    onClick={() => handleClick(character)}
+                                >
+                                    🤍
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                ))}
+                <div>
+                    <ul className="favorites-card">
+                        {favorite.favorites.map(favorite => (
+                            <li key={favorite.id}>
+                                {favorite.name}
+                            </li>
+                        ))}
                     </ul>
                 </div>
-            ))}
-            <div>
-                <ul className="favorites-card">
-                    {favorite.favorites.map(favorite => (
-                        <li key={favorite.id}>
-                            {favorite.name}
-                        </li>
-                    ))}
-                </ul>
             </div>
-        </div>
+        </>
     )
 }
 
